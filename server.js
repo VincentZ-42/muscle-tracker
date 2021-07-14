8fc2186e8727e6b6a5532ccc1119bfeb696cbae7
Vincent Zhao <VinZhao11@gmail.com>
2021-07-14 10:11:32 -0400

    7.14 morning

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+app.get ('/', (req, res) => res.send('API Runing'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.group(`Server started on port ${PORT}`));
--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,12 +1,50 @@
 import { FaTimes } from 'react-icons/fa'
 // import { FaRegEdit } from 'react-icons/fa'
 
+// Shows Weight, Distance, or Duration of Workout 
+// displayed next to Workout name
+const showInfo = (workout) => {
+  let output = '';
+
+  if (Number(workout.weight) > 0) {
+    output += ` - ${workout.weight} ${workout.weightUnit}`;
+  }
+  if (Number(workout.distance) > 0) {
+    output += ` - ${workout.distance} ${workout.distanceUnit}`
+  }
+  if (Number(workout.duration) > 0) {
+    output += ` - ${workout.duration} ${workout.durationUnit}`
+  }
+
+  return output;
+};
+
+// Creates a single button
+const makeButton = (workout) => {
+  return (
+    <button conClick={() => console.log('I have been clicked')}>
+      {workout.reps}
+    </button>
+  );
+};
+
+// Creates an array of buttons
+// that match the number of sets/reps in the workout
+const createButtons = (workout) => {
+  const sets = Number(workout.sets);
+  let btnGroup = [];
+  for (let i = 0; i < sets; i++) {
+    btnGroup.push(makeButton(workout));
+  }
+  return btnGroup;
+}
+
 const Workout = ({ workout, onDelete, onEdit }) => {
   return (
     <>
       <div className={`workout`}>
         <h3>
-          {workout.text}
+          {workout.text}{showInfo(workout)}
           {/* <FaRegEdit style={{
             color: 'blue',
             cursor: 'pointer' }}
@@ -18,13 +56,9 @@ const Workout = ({ workout, onDelete, onEdit }) => {
             onClick={() => onDelete(workout.id)}
           />
         </h3>
-        <p>{workout.sets ? 'Sets = ' + workout.sets : '' }</p>
-        <p>{workout.reps ? 'Reps = ' + workout.reps : '' }</p>
-        <p>{workout.weight ? 'Weight = ' + workout.weight : '' }</p>
-        <p>{workout.distance ? 'Distance = ' + workout.distance : '' }</p>
-        <p>{workout.duration ? 'Duration = ' + workout.duration : '' }</p>
-        <p>[ {workout.weightUnit}, {workout.distanceUnit}, {workout.durationUnit} ]</p>
-        <p>{'Muscles = ' + workout.muscles.join(' | ')}</p>
+        <div className='button-holder' id='setBtns'>
+          {createButtons(workout)}
+        </div>
         <p>{workout.comments ? 'Comments = ' + workout.comments : '' }</p>
       </div>
     </>
